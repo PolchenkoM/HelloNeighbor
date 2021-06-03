60c62425301bb98b14557656f41a99266fc4441c
Daniil <a336nm@gmail.com>
2021-06-03 19:39:11 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import UserMenuSider from "./components/UserMenuSider/UserMenuSider";
+
 
 function App() {
   return (
     <div className="App">
-     
+        <UserMenuSider />
+
     </div>
   );
 }
