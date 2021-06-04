d95cbeb14b4d6cac2e29bd832b432195ec04f5f1
PolchenkoM <maximus9279258@gmail.com>
2021-06-04 17:19:14 +0300

    Baloon in map

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require("mongoose");
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+const User = model("User", userSchema);
+module.exports = User;
